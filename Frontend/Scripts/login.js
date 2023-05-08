cb5f9c48e0b92377c5a6f13756d550d44b653fad
Debraj Roy <debrajroy154@gmail.com>
2023-05-08 16:33:41 +0530

    login

--- Frontend/Scripts/login.js
+++ Frontend/Scripts/login.js
@@ -1,14 +1,100 @@
-let signup = document.querySelector(".signup");
-let login = document.querySelector(".login");
-let slider = document.querySelector(".slider");
-let formSection = document.querySelector(".form-section");
+let inputloginform = document.querySelector(".loginform");
 
-signup.addEventListener("click", () => {
-	slider.classList.add("moveslider");
-	formSection.classList.add("form-section-move");
-});
+let apiuser="http://localhost:3000/user/"
 
-login.addEventListener("click", () => {
-	slider.classList.remove("moveslider");
-	formSection.classList.remove("form-section-move");
-});
+inputloginform.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let input_tags = document.querySelectorAll(".loginform input");
+    let obj = {};
+    obj.email = input_tags[0].value;
+    obj.password = input_tags[1].value;
+    login(obj)
+})
+
+async function login(obj) {
+    try {
+        let responce = await fetch(`${apiuser}login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(obj)
+        })
+
+        let res=await responce.json()
+
+        console.log(res);
+
+        if(res.status=="success"){
+            
+            localStorage.setItem("auth_token", JSON.stringify(res.token));
+            // let name=res.name;
+            localStorage.setItem("name", JSON.stringify(res.name));
+            // alert("login successfull");
+
+
+            // document.cookie=`token=${res.token}`;
+            // document.cookie=`token=${res.name}`;
+
+
+            window.location.href = "http://localhost:3000/chess/"
+
+        }
+
+          else {
+            alert("wrong credentials")
+        }
+    }
+    catch (err) {
+        alert("wrong credentials")
+        console.log("Some error");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// async function login(obj) {
+//     try {
+//         let login_data = await fetch(`${apiuser}login`, {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify(obj)
+//         })
+//         if (login_data.ok) {
+//             let token = await login_data.json();
+//             localStorage.setItem("auth_token", token.token);
+
+//             alert("login successfull");
+//             window.location.href = "userprofile.html"
+//         } else {
+//             alert("wrong credentials")
+//         }
+//     }
+//     catch (err) {
+//         alert("wrong credentials")
+//         console.log("Some error");
+//     }
+// }
+
+
+
+
