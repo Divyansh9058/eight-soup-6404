e12f5d1a0e206dff357e3f2f661e390e8712392c
Debraj Roy <debrajroy154@gmail.com>
2023-05-09 10:29:56 +0530

    front

--- Frontend/Scripts/register.js
+++ Frontend/Scripts/register.js
@@ -1,45 +1,44 @@
 let submit_register = document.querySelector(".registerform");
-let RegisterUserDataBase =
-  JSON.parse(localStorage.getItem("userdatabase")) || [];
+let apiuser = "http://localhost:3000/user/";
 
-let apiuser="http://localhost:3000/user/"
+submit_register.addEventListener("submit", async (event) => {
+  event.preventDefault();
 
-submit_register.addEventListener("submit", (event) => {
-    event.preventDefault();
+  let input_tags = document.querySelectorAll(".registerform input");
+  let obj = {
+    name: input_tags[0].value,
+    email: input_tags[1].value,
+    password: input_tags[2].value,
+  };
 
-    let input_tags = document.querySelectorAll(".registerform input");
-
-    console.log(input_tags)
-
-    let obj = {};
-    obj.name = input_tags[0].value;
-    
-    obj.email = input_tags[1].value;
-    obj.password = input_tags[2].value;
-
-    register(obj);
-})
-
-async function register(obj) {
-    try {
-        let response = await fetch(`${apiuser}/signup`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(obj)
-        });
-
-        let res=await response.json()
-        console.log(res)
-
-        if (res.status == "success") {
-           
-            alert("user has been succesfully created");
-            window.location.href ="./login.html"
-        }
+  try {
+    // Save user data to MongoDB
+    let response = await fetch(`${apiuser}/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(obj),
+    });
+    let res = await response.json();
+    console.log(res);
+    if (res.status == "success") {
+      alert("User has been successfully created");
     }
-    catch (err) {
-        console.log("some error");
-    }
-}
+  } catch (err) {
+    console.log("Error saving user data to MongoDB");
+  }
+
+  try {
+    // Save user data to local storage
+    let RegisterUserDataBase =
+      JSON.parse(localStorage.getItem("userdatabase")) || [];
+    RegisterUserDataBase.push(obj);
+    localStorage.setItem("userdatabase", JSON.stringify(RegisterUserDataBase));
+    alert("User data has been successfully saved to local storage");
+  } catch (err) {
+    console.log("Error saving user data to local storage");
+  }
+
+  window.location.href = "./login.html";
+});
